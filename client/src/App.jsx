15b0b32d4cd3ad1a9dd7b2afd8d6c64e2def8561
Shahin-Riyaz <dev.shahinriyas@gmail.com>
2025-03-20 00:45:27 +0530

    Create project page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import About from "./pages/Project";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Project from "./pages/Project";
